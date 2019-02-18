2a5a41adfb51cb3dcda5fee04633900acdcf29ad
rejeess <rejeess@gmail.com>
2019-02-18 11:38:52 -0600

    adding first commit

---
+++ myscript.js
@@ -0,0 +1,50 @@
+
+var pageDisplay = document.getElementById("demo");
+var btn = document.getElementById("nowPlaying");
+var btn1 = document.getElementById("comingSoon");
+var btn2 = document.getElementById("all");
+var api = "http://api.kendomobilebook.com/api/Movies/GetMovieList/?listType=";
+
+
+
+btn.addEventListener("click", function() {
+ajaxcall(0);
+
+})
+  
+btn1.addEventListener("click", function() {
+    ajaxcall(1);
+    
+    })
+
+btn2.addEventListener("click", function() {
+        ajaxcall(2);
+        
+        })
+function ajaxcall(type){
+    var urlRequest = new XMLHttpRequest();
+    urlRequest.open('GET', api+type);
+    urlRequest.onload = function() {
+        //console.log(urlRequest.responseText);
+        var movieData = JSON.parse(urlRequest.responseText);
+        //console.log(movieData[0]);
+        loadHTML(movieData);
+    };
+    urlRequest.send();
+}
+   
+
+
+
+function loadHTML(data){
+    var htmlString = "";
+
+    for (i = 0; i < data.length; i++){
+        htmlString += "<img class= 'movie-img' src=" + data[i].Image + "/>" +"<h4 id='mov-name'>" + data[i].Name + "</h4>";
+        htmlString += "<p class='mov-text'>" + data[i].Genre+"," +" "+data[i].Length+" "+"Mins"+"  "+  data[i].Rating +"</p>";
+        htmlString += "<p class='mov-text'>" + data[i].LeadStars + "</p>";
+
+    }
+
+    pageDisplay.insertAdjacentHTML('beforeend', htmlString);
+}
