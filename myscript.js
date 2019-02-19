4e835b7cfe9e4c83dc5f6ff734b564a5b3055043
ambilireghu <ambilireghu@gmail.com>
2019-02-18 18:40:03 -0600

    Fix for the image and the button loading

--- myscript.js
+++ myscript.js
@@ -1,30 +1,27 @@
 
 var pageDisplay = document.getElementById("demo");
-var btn = document.getElementById("nowPlaying");
-var btn1 = document.getElementById("comingSoon");
-var btn2 = document.getElementById("all");
-var api = "http://api.kendomobilebook.com/api/Movies/GetMovieList/?listType=";
+var btn         = document.getElementById("nowPlaying");
+var btn1        = document.getElementById("comingSoon");
+var btn2        = document.getElementById("all");
+var api         = "http://api.kendomobilebook.com/api/Movies/GetMovieList/?listType=";
 
 
+btn.addEventListener("click", function () {
+    ajaxcall(0);
+});
 
-btn.addEventListener("click", function() {
-ajaxcall(0);
-
-})
-  
-btn1.addEventListener("click", function() {
+btn1.addEventListener("click", function () {
     ajaxcall(1);
-    
-    })
+});
 
-btn2.addEventListener("click", function() {
-        ajaxcall(2);
-        
-        })
-function ajaxcall(type){
+btn2.addEventListener("click", function () {
+    ajaxcall(2);
+});
+
+function ajaxcall(type) {
     var urlRequest = new XMLHttpRequest();
-    urlRequest.open('GET', api+type);
-    urlRequest.onload = function() {
+    urlRequest.open('GET', api + type);
+    urlRequest.onload = function () {
         //console.log(urlRequest.responseText);
         var movieData = JSON.parse(urlRequest.responseText);
         //console.log(movieData[0]);
@@ -32,19 +29,21 @@ function ajaxcall(type){
     };
     urlRequest.send();
 }
+
+
+function loadHTML(data) {
+
    
-
-
-
-function loadHTML(data){
     var htmlString = "";
 
-    for (i = 0; i < data.length; i++){
-        htmlString += "<img class= 'movie-img' src=" + data[i].Image + "/>" +"<h4 id='mov-name'>" + data[i].Name + "</h4>";
-        htmlString += "<p class='mov-text'>" + data[i].Genre+"," +" "+data[i].Length+" "+"Mins"+"  "+  data[i].Rating +"</p>";
+    for (i = 0; i < data.length; i++) {
+        // htmlString += "<img class= 'movie-img' src=" + data[i].Image + "/>" + "<h4 id='mov-name'>" + data[i].Name + "</h4>";
+        htmlString += "<img class= 'movie-img' src=" + data[i].Image + " />" + "<h4 id='mov-name'>" + data[i].Name + "</h4>";
+        htmlString += "<p class='mov-text'>" + data[i].Genre + "," + " " + data[i].Length + " " + "Mins" + "  " + data[i].Rating + "</p>";
         htmlString += "<p class='mov-text'>" + data[i].LeadStars + "</p>";
 
     }
-
-    pageDisplay.insertAdjacentHTML('beforeend', htmlString);
+    console.log(htmlString);
+    // pageDisplay.insertAdjacentHTML(htmlString); This line was creating the issue .
+    pageDisplay.innerHTML=htmlString;
 }
